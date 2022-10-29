60cc14b3ca487ca473bce91b2db2897d3b85d04f
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-10-30 00:27:30 +0600

    login in with google

--- src/Pages/Shared/RigitSideNav/RightSideNav.js
+++ src/Pages/Shared/RigitSideNav/RightSideNav.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch } from "react-icons/fa";
 import ListGroup from 'react-bootstrap/ListGroup';
 import BrandCarousel from '../BrandCarousel/BrandCarousel';
+import { GoogleAuthProvider } from 'firebase/auth';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const RightSideNav = () => {
+    const {providerLogin} = useContext(AuthContext);
+
+    const googleProvider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () =>{
+        providerLogin(googleProvider)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+        })
+    }
+
     return (
         <div>
             <ButtonGroup vertical>
-                <Button className='mb-2' variant="outline-primary"><FaGoogle></FaGoogle>  Login with Google</Button>
+                <Button onClick={handleGoogleSignIn} className='mb-2' variant="outline-primary"><FaGoogle></FaGoogle>  Login with Google</Button>
                 <Button variant="outline-dark"><FaGithub></FaGithub>  Login with Github</Button>
             </ButtonGroup>
             <div className='mt-4'>
