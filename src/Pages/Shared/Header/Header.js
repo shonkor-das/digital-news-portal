e8c116464ac991a27aaeb3fad8696a0535a22d69
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-10-29 21:32:21 +0600

    news title added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,13 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 import LeftSideNav from '../LeftSideNav/LeftSideNav';
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect className='mb-3' expand="lg" bg="light" variant="light">
             <Container>
-                <Navbar.Brand href="#home">News Portal</Navbar.Brand>
+                <Navbar.Brand><Link to='/'>News Portal</Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
